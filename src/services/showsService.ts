e33ca884ad08f512367124ee8c503742f4b3405a
Danny <danny.lucas@live.nl>
2023-07-23 22:56:39 +0200

    Updated typing since the API returns multiple formats from the same endpoint

--- src/services/showsService.ts
+++ src/services/showsService.ts
@@ -1,32 +1,54 @@
+import {AxiosResponse} from 'axios';
 import api from '../api/axios';
 import {Show} from '../models/show';
 
-export interface ShowResponse {
+interface ShowResponse {
   Search: Show[];
   totalResults: string;
   Response?: string;
 }
 
+interface SingleShow {
+  title: string;
+  year: string;
+  rated: string;
+  released: string;
+  runtime: string;
+  genre: string;
+  director: string;
+  writer: string;
+  actors: string;
+  plot: string;
+  language: string;
+  country: string;
+  awards: string;
+  poster: string;
+  ratings: { [key: string]: string }[];
+  metascore: string;
+  imdbRating: string;
+  imdbVotes: string;
+  imdbID: string;
+  type: string;
+  totalSeasons: string;
+  response: string;
+}
+
+// This could also be done in an interceptor, but the API seems to return different formats depending on the parameters and since there is a
+// time limit I do not want to go through every possible option and cover those.
+const handleResponse = (result: AxiosResponse<any, any>) => {
+  const {data} = result;
+  // This would need better typing overall.
+  if (data.Error) {
+    throw new Error('API returned an error within the body');
+  }
+  return data;
+};
+
 export const fetchShows = (s: string): Promise<ShowResponse> => api.get('', {params: {s}})
-  .then((result) => {
-    const {data} = result;
-    // This would need better typing overall.
-    if (data.Error) {
-      throw new Error('API returned an error within the body');
-    }
-    return data as ShowResponse;
-  })
-  // Very minimal error handling
+  // Typing is handled by the return type of the function
+  .then(handleResponse)
   .catch(() => ({Search: [], totalResults: '0'} as ShowResponse));
 
-export const getShow = (i: string): Promise<ShowResponse> => api.get('', {params: {i}})
-  .then((result) => {
-    const {data} = result;
-    // This would need better typing overall.
-    if (data.Error) {
-      throw new Error('API returned an error within the body');
-    }
-    return data as ShowResponse;
-  })
-  // Very minimal error handling
-  .catch(() => ({Search: [], totalResults: '0'} as ShowResponse));
+export const getShow = (i: string): Promise<SingleShow> => api.get('', {params: {i}})
+  .then(handleResponse)
+  .catch(() => ({} as SingleShow));
